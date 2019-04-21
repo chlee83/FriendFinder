83810e358bb83a7e438aa4ff505914c132fa3ed0
Hobbes <chilee83@gmail.com>
2019-04-20 22:40:26 -0400

    fixed bugs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Friend Finder
+
+Friend Finder will help you find a new friend that is a good match for you. 
+
+Take the survey. Make sure you answer honestly!
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Route Files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 // Starts the server to begin listening
